b6335c89079b732887b807b9c18903cbba7c11b2
Vyas Raina <sayvaniar@gmail.com>
2022-06-01 02:01:07 +0100

    imdb data

--- data/dataloader.py
+++ data/dataloader.py
@@ -1,6 +1,10 @@
 from data.load_twitter import TwitterLoader
+from data.load_imdb import IMDBLoader
 
 def load_data(dataset, filepath=None, type=None):
     if dataset == 'twitter':
         dl =  TwitterLoader()
+        return dl.get_data(filepath)
+    if dataset == 'imdb':
+        dl = IMDBLoader()
         return dl.get_data(filepath)
