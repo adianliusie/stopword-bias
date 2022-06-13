578d4cc517f26deb9171be46e4bbd330bcad46b9
Vyas Raina <sayvaniar@gmail.com>
2022-06-13 17:46:53 +0100

    debug

--- plot_retention.py
+++ plot_retention.py
@@ -86,7 +86,8 @@ if __name__ == '__main__':
         # Plot
         plt.plot(fracs, pos_class_fracs_pred[args.class_ind], label=f'{mdl_name}')
 
-    print(pos_class_fracs_label[1][:500])
+    print(len(pos_class_fracs_label[args.class_ind]))
+    print(pos_class_fracs_label[args.class_ind][:500])
     plt.plot(fracs, pos_class_fracs_label[args.class_ind], label=f'label')
     plt.plot(fracs, fracs, label=f'random', linestyle='dashed')
     plt.plot(fracs, pos_class_fracs_ideal[args.class_ind], label=f'ideal', linestyle='dashed')
