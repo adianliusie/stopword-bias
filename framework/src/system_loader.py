6a9cb824061bc2bdf562dc2dc88ec6f64284cb37
adianliusie <al826@million.eng.cam.ac.uk>
2022-06-13 13:25:37 +0100

    added ensemble loader

--- framework/src/system_loader.py
+++ framework/src/system_loader.py
@@ -1,12 +1,40 @@
 import torch.nn.functional as F
-from tqdm import tqdm
 import numpy as np
+import os
+
+from tqdm import tqdm
 
 from .trainer import Trainer
 from .utils.torch_utils import no_grad
 from .utils.data_utils import load_data
 from .helpers import DirHelper
 
+class EnsembleLoader():
+    def __init__(self, exp_path:str):
+        self.exp_path = exp_path
+        self.paths  = [f'{exp_path}/{seed}' for seed in os.listdir(exp_path)]
+        self.seeds  = [SystemLoader(seed_path) for seed_path in self.paths]
+    
+    def load_probs(self, data_name, mode)->dict:
+        seed_probs = [seed.load_probs(data_name, mode) for seed in self.seeds]
+
+        conv_ids = seed_probs[0].keys()
+        assert all([i.keys() == conv_ids for i in seed_probs])
+
+        ensemble = {}
+        for conv_id in conv_ids:
+            probs = [seed[conv_id] for seed in seed_probs]
+            probs = np.mean(probs, axis=0)
+            ensemble[conv_id] = probs
+        return ensemble    
+
+    def load_preds(self, data_name, mode):
+        probs = self.load_probs(data_name, mode)
+        preds = {}
+        for k, probs in probs.items():
+            preds[k] = int(np.argmax(probs, axis=-1))
+        return preds
+    
 class SystemLoader(Trainer):
     """Base loader class- the inherited class inherits
        the Trainer so has all experiment methods"""
@@ -27,11 +55,9 @@ class SystemLoader(Trainer):
 
     def load_preds(self, data_name, mode):
         probs = self.load_probs(data_name, mode)
-        
         preds = {}
         for k, probs in probs.items():
-            preds[k] = int(np.argmax(probs, axis=-1))
-        
+            preds[k] = int(np.argmax(probs, axis=-1))  
         return preds
         
     def load_probs(self, data_name, mode):
@@ -64,13 +90,23 @@ class SystemLoader(Trainer):
             probabilties[sample_id] = y.cpu().numpy()
         return probabilties
 
+    def formatted_probs(self, formatting, data_name, mode='test'):
+        """ evaluates the model in the different formatting set up"""
+        if formatting == self.formatting: 
+            return self.load_probs(data_name, mode)
+        else:
+            og_formatting = self.formatting
+            data_loader.formatting = formatting
+            
+        
     def _get_eval_batches(self, data_name, mode='test'):
         #get eval data- data_loader returns (train, dev, test) so index
         eval_data = self.data_loader.get_data_split(data_name, mode)
         eval_batches = self.batcher(data=eval_data, bsz=1, shuffle=False)
         return eval_batches
 
-    def load_labels(self, data_name, mode='test'):
+    @staticmethod
+    def load_labels(data_name, mode='test'):
         split_index = {'train':0, 'dev':1, 'test':2}
         eval_data = load_data(data_name)[split_index[mode]]
         
@@ -79,7 +115,8 @@ class SystemLoader(Trainer):
             labels_dict[k] = ex['label']
         return labels_dict
 
-    def load_inputs(self, data_name, mode='test'):
+    @staticmethod
+    def load_inputs(data_name, mode='test'):
         split_index = {'train':0, 'dev':1, 'test':2}
         eval_data = load_data(data_name)[split_index[mode]]
         
