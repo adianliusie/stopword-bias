19dde562f4d0525999122d43c15461f79c6570d9
Vyas Raina <sayvaniar@gmail.com>
2022-06-14 00:19:43 +0100

    debug

--- plot_retention.py
+++ plot_retention.py
@@ -65,8 +65,6 @@ if __name__ == '__main__':
         if mdl_name == 'stopgram':
             stp = NllModel()
             preds = stp.load_preds(sentences)
-            pred_labs = [0 if p<1 else 1 for p in preds]
-            print('Accuracy', accuracy_score(pred_labs, labels))
         else:
             preds_dict  = system.load_preds(args.dataname,  mode=args.mode)
             preds = [preds_dict[i] for i in range(len(preds_dict))]
@@ -82,6 +80,8 @@ if __name__ == '__main__':
         feats_pred = RG_pred.get_feat(args.feat)
         fracs, pos_class_fracs_pred = RG_pred.retention_plot(feats_pred, cum)
         feats_label = RG_label.get_feat(args.feat)
+        pred_labs = [0 if p<1 else 1 for p in feats_label]
+        print('Accuracy', accuracy_score(pred_labs, labels))
         _, pos_class_fracs_label = RG_label.retention_plot(feats_label, cum)
         _, pos_class_fracs_ideal = RG_label.retention_plot(labels, cum)
 
