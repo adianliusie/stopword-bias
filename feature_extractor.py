6d8b5e53401d7c475d727525ba8bb941c0ec1798
Vyas Raina <sayvaniar@gmail.com>
2022-06-14 01:07:00 +0100

    dbug

--- feature_extractor.py
+++ feature_extractor.py
@@ -56,7 +56,7 @@ class RetentionGenerator(FeatureExtractor):
         ordering = np.argsort(np.asarray(features))
         ordered_ys = np.asarray(self.ys)[ordering]
         # ordered_ys = [x for _,x in sorted(zip(features, self.ys))]
-        pred_labs = [0 if p<0 else 1 for p in features[ordering]]
+        pred_labs = [0 if p<0 else 1 for p in np.asarray(features)[ordering]]
         print('Accuracy', accuracy_score(pred_labs, ordered_ys))
 
         # retention plot per class
